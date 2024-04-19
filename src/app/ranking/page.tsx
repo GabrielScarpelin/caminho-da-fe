059e8a9d124af767d929e79ea68370abeb5f4204
GabrielScarpelinDiniz <159063078+GabrielScarpelinDiniz@users.noreply.github.com>
2024-04-19 08:47:13 -0300

    Suspense envolving at ranking

--- src/app/ranking/page.tsx
+++ src/app/ranking/page.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 import rankingImage from "../../assets/rankingImage.png";
 import podioImage from "../../assets/podio.png";
 import styles from "./page.module.css";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 export default function Ranking(){
     const searchParams = useSearchParams()
-    const router = useRouter();
     return (
-        <>
+        <Suspense >
             <div style={{ display: "flex", height: "100%", alignItems: "center", justifyContent: "space-between" }}>
                 <Image src={rankingImage} alt="Imagem da pomba na tela de ranking"/>
                 <div style={{ marginRight: 108, display: "flex", flexDirection: "column", position: "relative"}}>
@@ -19,6 +19,6 @@ export default function Ranking(){
                     <h2 style={{ bottom: "35%", right: 0}} className={styles.podioText}>{searchParams.get("third")}</h2>
                 </div>
             </div>
-        </>
+        </ Suspense>
     )
 }
