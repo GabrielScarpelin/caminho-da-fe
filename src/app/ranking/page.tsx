70e8acd33c017b2ff02df5ef971541042c508637
GabrielScarpelinDiniz <159063078+GabrielScarpelinDiniz@users.noreply.github.com>
2024-04-19 09:04:39 -0300

    Resolving useSearchParam error

--- src/app/ranking/page.tsx
+++ src/app/ranking/page.tsx
@@ -6,19 +6,30 @@ import styles from "./page.module.css";
 import { useSearchParams } from "next/navigation";
 import { Suspense } from "react";
 export default function Ranking(){
-    const searchParams = useSearchParams()
     return (
-        <Suspense >
+        <>
             <div style={{ display: "flex", height: "100%", alignItems: "center", justifyContent: "space-between" }}>
                 <Image src={rankingImage} alt="Imagem da pomba na tela de ranking"/>
                 <div style={{ marginRight: 108, display: "flex", flexDirection: "column", position: "relative"}}>
                     <h1 className={styles.title}>Ranking</h1>
                     <Image src={podioImage} alt="Imagem do pódio"/>
-                    <h2 style={{ bottom: "35%"}} className={styles.podioText}>{searchParams.get("second")}</h2>
-                    <h2 style={{ bottom: "55%", left: "50%", translate: "-50%"}} className={styles.podioText}>{searchParams.get("winner")}</h2>
-                    <h2 style={{ bottom: "35%", right: 0}} className={styles.podioText}>{searchParams.get("third")}</h2>
+                    <Suspense>
+                        <PodiumText />
+                    </Suspense>
+                    
                 </div>
             </div>
-        </ Suspense>
+        </>
+    )
+}
+
+function PodiumText(){
+    const searchParams = useSearchParams();
+    return (
+        <>
+            <h2 style={{ bottom: "35%"}} className={styles.podioText}>{searchParams.get("second")}</h2>
+            <h2 style={{ bottom: "55%", left: "50%", translate: "-50%"}} className={styles.podioText}>{searchParams.get("winner")}</h2>
+            <h2 style={{ bottom: "35%", right: 0}} className={styles.podioText}>{searchParams.get("third")}</h2>
+        </>
     )
 }
